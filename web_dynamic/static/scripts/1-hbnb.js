771efd99975b580990de854a55a284717ba62746
root <eme.gaby.do@gmail.com>
2022-05-28 22:45:22 +0000

    task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,2 +1,16 @@
-let isChecked = $('#cajita').is(':checked');
-alert("check???");
+$(document).ready(function () {
+  let all_checked = {};
+  $(document).on('change', "input[type='checkbox']", function () {
+    if (this.checked) {
+      all_checked[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete all_checked[$(this).data('id')];
+    }
+    let lista = Object.values(all_checked);
+    if (lista.length > 0) {
+      $('div.amenities > h4').text(Object.values(all_checked).join(', '));
+    } else {
+      $('div.amenities > h4').html('&nbsp;');
+    }
+  });
+});
